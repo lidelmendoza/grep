383fe5ea9a124e588481bfee74ad981c6bb27142
Lidel Mendoza <lidelmendoza9@csu.fullerton.edu>
2019-03-18 12:36:21 -0700

    Initial Commit

---
+++ grep3/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  grep3
+//
+//  Created by Lidel Mendoza on 3/18/19.
+//  Copyright © 2019 Lidel Mendoza. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
